da0361d7bb6f219baf829fda11aa700efeff00fe
benk861 <benk865@gmail.com>
2022-12-29 23:48:46 +0100

    Initial commit

---
+++ Source/ASCEND/ASCENDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ASCENDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASCEND_API AASCENDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
